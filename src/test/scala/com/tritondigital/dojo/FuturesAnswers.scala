001b10c43162bd2cd881aa0ee2f61e3de8dec5a4
Sebastian Meric de Bellefon <Sebastian.MericdeBellefon@tritondigital.com>
2016-02-25 19:45:48 -0500

    futures

--- src/test/scala/com/tritondigital/dojo/FuturesAnswers.scala
+++ src/test/scala/com/tritondigital/dojo/FuturesAnswers.scala
@@ -1,14 +1,17 @@
 package com.tritondigital.dojo
 
+
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{FunSuite, Matchers}
 import org.scalautils.TypeCheckedTripleEquals
 
-import scala.concurrent.duration._
 import scala.concurrent.{Future, blocking}
+import scala.concurrent.duration._
+
+import org.scalatest.{Matchers, FunSuite}
+
 import scala.language.postfixOps
-/*
-class Futures extends FunSuite with ScalaFutures with Matchers with TypeCheckedTripleEquals {
+
+class FuturesAnswers extends FunSuite with ScalaFutures with Matchers with TypeCheckedTripleEquals {
 
   // A Future[A] represents a computation that will eventually return a value of type A. Or a Throwable.
   //  futures (time + failure) are simpler than actors (time + failure + state), but actors are strictly more expressive
@@ -58,21 +61,21 @@ class Futures extends FunSuite with ScalaFutures with Matchers with TypeCheckedT
 
   test("Check the future's value") {
     val f = fut(1)
-    assert(f.??? === 1)
+    assert(f.futureValue === 1)
   }
 
   // Hint: What is the type of f.failed?
   test("Check that the future failed") {
     val f = failed()
     intercept[RuntimeException] {
-      assert(f.??? === ???)
+      assert(f.futureValue === ???)
     }
-    assert(f.??? === "reason")
+    assert(f.failed.futureValue.getMessage === "reason")
   }
 
 
   test("Check that the future is ready within 500 milliseconds (or not)") {
-    def check(f: Future[Int]): Boolean = f.???(500 millis)
+    def check(f: Future[Int]): Boolean = f.isReadyWithin(500 millis)
     val fast = fut(1, wait = 100 millis)
     assert(check(fast))
     val slow = fut(1, wait = 1 second)
@@ -82,14 +85,15 @@ class Futures extends FunSuite with ScalaFutures with Matchers with TypeCheckedT
   test("Race between two futures (i.e keep the fastest one)") {
     val fast = fut(1, wait = 100 millis)
     val slow = fut(2, wait = 1 second)
-    val faster = Future.???(Seq(slow, fast))
+    val faster = Future.firstCompletedOf(Seq(slow, fast))
     assert(faster.isReadyWithin(500 millis))
     assert(faster.futureValue === 1)
   }
 
 
   def withTimeoutDefault[A](f: Future[A], d: Duration, default: A): Future[A] = {
-    ???
+    val futs = Seq(f, fut(default, d))
+    Future.firstCompletedOf(futs)
   }
 
   test("Use a race to replace a timeout with a default value (withTimeoutDefault)") {
@@ -101,7 +105,7 @@ class Futures extends FunSuite with ScalaFutures with Matchers with TypeCheckedT
 
 
   def withFailureDefault[A](f: Future[A], default: A): Future[A] = {
-    f.???
+    f.fallbackTo(Future(default))
   }
 
   test("Fallback if a future fails (withFailureDefault)") {
@@ -111,7 +115,7 @@ class Futures extends FunSuite with ScalaFutures with Matchers with TypeCheckedT
   }
 
   def withFailureDefaultNone[A](f: Future[A]): Future[Option[A]] = {
-    f.???
+    withFailureDefault(f.map(Some(_)), None) // Or directly:  f.map(Some(_)).fallbackTo(Future(None))
   }
 
   test("Fallback if a future fails (withFailureDefaultNone)") {
@@ -121,9 +125,10 @@ class Futures extends FunSuite with ScalaFutures with Matchers with TypeCheckedT
   }
 
 
+
   test("Recover from a specific exception (with another future)") {
     val bad1 = failed()
-    val chain = bad1.??? {
+    val chain = bad1.recoverWith {
       case e: RuntimeException => fut(0) // More specific than fallbackTo, but also more verbose
     }
     assert(chain.futureValue === 0)
@@ -140,19 +145,19 @@ class Futures extends FunSuite with ScalaFutures with Matchers with TypeCheckedT
   // Hint: Future is a Functor, just as List/Seq/Option
   test("Map a function on a future") {
     val f: Future[Int] = fut(1)
-    val f2: Future[String] = ???
+    val f2: Future[String] = f.map(_.toString)
     assert(f2.futureValue === "1")
   }
 
   test("Map twice on a future") {
     val f = fut(1)
-    val f2 = f.???(_.toString).???(_ ++ "!")
+    val f2 = f.map(_.toString).map(_ ++ "!")
     assert(f2.futureValue === "1!")
   }
 
   test("Print the content of a future if it succeeded") {
     val f = fut(1)
-    f.???
+    f.foreach(println)
   }
 
 
@@ -161,13 +166,13 @@ class Futures extends FunSuite with ScalaFutures with Matchers with TypeCheckedT
   test("Run a list of futures") {
     val futs: Seq[Future[Int]] = (1 to 50).map(fut(_))
     val futs2: Seq[Future[String]] = futs.map(f => f.map(_.toString))
-    assert(Future.???(futs2).futureValue === (1 to 50).map(_.toString)) // Wait for them in parallel
+    assert(Future.sequence(futs2).futureValue === (1 to 50).map(_.toString)) // Wait for them in parallel
   }
 
   // Works over any Traversable type
   test("Run the same future constructor over a list of values (we need to 'traverse' a sequence of Int))") {
     def f(n: Int) = fut(n + 1)
-    assert(Future.???(1 to 50)(f).futureValue === (2 to 51)) // Create them and wait for them in parallel
+    assert(Future.traverse(1 to 50)(f).futureValue === (2 to 51)) // Create them and wait for them in parallel
   }
 
 
@@ -177,16 +182,20 @@ class Futures extends FunSuite with ScalaFutures with Matchers with TypeCheckedT
     def plus1(i: Int): Int = i + 1
     val d = 100 millis
     val f = for {
-      ???
-    } yield ???
+      a <- fut(1, d)
+      b <- fut(plus1(a), d) // the for block allows reusing some previous result
+      c <- fut(plus1(b), d)
+    } yield c
     assert(f.futureValue === 3)
   }
 
 
   test("Chain several futures in a for block, with one of them failing") {
     val f = for {
-      ???
-    } yield ???
+      a <- fut(1)
+      b <- failed()
+      c <- fut(b + 1) // This line will not be executed
+    } yield c
     intercept[RuntimeException] {
       assert(f.futureValue === 100)
     }
@@ -210,4 +219,4 @@ class Futures extends FunSuite with ScalaFutures with Matchers with TypeCheckedT
   */
 
 
-}*/
+}
